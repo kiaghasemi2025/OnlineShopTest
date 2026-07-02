ec87594042ac2ff4149a6e2700d5edf838ee0282
kiaghasemi2025 <kiaghasemi2025@gmail.com>
2026-07-02 14:50:50 +0330

    creat mini api

--- practice/practice.js
+++ practice/practice.js
@@ -1,15 +1,65 @@
+const multer = require('multer')
+const express = require('express')
 const PDFDocument = require('pdfkit')
 const fs = require('fs')
 const path = require('path')
+const app = express()
+const router = express.Router()
 
-let invoicePath = path.join('files','invoices','invoice.pdf')
+let discStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
 
-let doc = new PDFDocument()
+let fileFilter = (req, file, cb) => {
 
-doc.pipe(fs.createWriteStream(invoicePath))
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'application/pdf') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
 
-// title
-doc.fontSize(20).text('This Is Title')
-doc.text('Hello World')
-doc.end()
+}
 
+app.use(multer({
+    storage: discStorage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 2000000 }
+}).single('images'))
+
+
+// get download
+
+exports.getDownloadPdf = (req, res) => {
+    pdfDoc = new PDFDocument()
+    let pdfName = 'simple.pdf'
+    let pdfPath = path.join('files', pdfName)
+
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'attachment; filename="' + pdfName + '"')
+    pdfDoc.pipe(fs.createWriteStream(pdfPath))
+    pdfDoc.pipe(res)
+    pdfDoc.text('Title')
+    pdfDoc.text('End')
+    pdfDoc.end()
+}
+
+// router 
+
+router.get('/invoice',isAuth,adminController.getDownloadPdf)
+
+// is Auth
+
+// (req,res,next) => {
+//     if(!req.session.isLoggedin){
+//         res.redirect('/login')
+//     }
+//     next()
+// }
